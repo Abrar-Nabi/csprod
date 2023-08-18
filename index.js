80ff555f5ea130a06bbfca7a204a07774475f03e
abrarnabi <117633785+Abrar-Nabi@users.noreply.github.com>
2023-08-18 10:23:10 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
-document.addEventListener('contextmenu', function(e) {
-    e.preventDefault();
+// document.addEventListener('contextmenu', function(e) {
+//     e.preventDefault();
+// });
+
+
+
+
+
+const obs = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry)
+    if (entry.isIntersecting){
+      entry.target.classList.add('showimg');
+    }
+    else{
+      entry.target.classList.remove('showimg');
+    }
+  });
 });
+
+const ourTeamPage = document.querySelectorAll('.image');
+ourTeamPage.forEach((el) => obs.observe(el));
+
+
+
+
+
+
 
 
 const bar = document.getElementById('bar');
@@ -18,12 +43,28 @@ if (close) {
 }
 
 
-// const hoverContainer = document.querySelector('.box-content');
+// const hoverContainer = document.querySelector('.box-content')
 //     hoverContainer.addEventListener('click', () => {
 //     hoverContainer.classList.toggle('opacityVisible');
 
 // });
 
-function show(classList) {
-    classList.toggle('opacityVisible');
-   }
+// function show(classList) {
+//     classList.toggle('opacityVisible');
+//    }
+
+
+
+// // My Code 
+function toggleClass(clickedDiv) {
+//   // Get all elements with the box-content class
+  const divs = document.querySelectorAll('.box-content');
+  
+//   // Remove the CSS class from all divs
+  divs.forEach(div => div.classList.remove('opacityVisible'));
+  
+//   // Add the CSS class to the clicked div
+  clickedDiv.classList.toggle('opacityVisible');
+}
+
+
